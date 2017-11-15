9137f21e05cdcddcd46449b9f18501773dfffa45
StarLikeRain <yaokangpeng@yaokangs-MacBook-Pro.local>
2017-11-15 21:40:13 +0800

    创建了项目，但是不知道配置好了没有

---
+++ main.c
@@ -0,0 +1,15 @@
+/**
+ * @author [StarLikeRain]
+ * @email [pengyaokang@icloud.com]
+ * @create date 15/11/2017
+ * @modify date 15/11/2017
+ * @desc [description]
+*/
+
+
+#include <stdio.h>
+
+int main(){
+    printf("helo world");
+    return 0;
+}
